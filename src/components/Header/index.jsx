48f566caa0661a1e1ab66c9740891fd878a77e07
Pedro Franca <pedrom.franca@hotmail.com>
2023-12-26 16:25:05 -0300

    feat: criação da  pagina home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { selectUser } from '../../redux/userSlice';
 
-function index() {
+const HeaderStyled = styled.header`
+width: 100%;
+height: 45px;
+border-bottom: 1px solid #E5E7EB;
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 0 20px;
+`;
+
+const ButtonStyled = styled.button`
+background-color: #EF4444;
+width: 39.24px;
+height: 28px;
+border-radius: 4px;
+padding: 3.5 8.24 4.5 8px;
+color: #FFFFFF;
+font-size: 14px;
+line-height: 20px;
+align-items: center;
+`;
+
+const UserStyled = styled.h1`
+  color: #6B7280;
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 400;
+  font-family: "Roboto", sans-serif;
+  margin-left: 75%;
+`;
+
+const TitleStyled = styled.h1`
+font-size: 18px;
+line-height: 28px;
+font-weight: 400;
+font-family: "Roboto", sans-serif;
+color: #0EA5E9;
+`;
+
+function Header() {
+  const user = useSelector(selectUser);
+
   return (
-    <div>index</div>
+    <HeaderStyled>
+      <TitleStyled>aluritter</TitleStyled>
+      <UserStyled>{user.userData.login}</UserStyled>
+      <ButtonStyled>sair</ButtonStyled>
+    </HeaderStyled>
   );
 }
 
-export default index;
+export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,156 @@
+import styled from 'styled-components';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import Header from '../../components/Header';
 import { selectUser } from '../../redux/userSlice';
 
+const ContainerSginStyled = styled.div`
+  background-color: #F3F4F6;
+  width: 100%;
+  height: 100vh;
+`;
+
+const TitleStyled = styled.h1`
+color: #4B5563;
+font-size: 14px;
+line-height: 20px;
+font-weight: 400;
+margin-bottom: 10px;
+margin-top: 50px;
+`;
+
+const ButtonStyled = styled.button`
+width: 66.49px;
+height: 40px; 
+border-radius: 4px;
+padding: 8.5 7.49 7.5 8px;
+background-color: #0EA5E9;
+font-family: "Roboto", sans-serif;
+color: #FFFFFF;
+font-size: 16px;
+line-height: 24px;
+`;
+
+const StyledTextarea = styled.textarea`
+  width: 1200px;
+  border: 1px solid #E5E7EB;
+  border-radius: 4px;
+  resize: none;
+  height: 114px;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const ContainerStyled = styled.div`
+display: flex;
+justify-content: space-between;
+margin-top: 15px;
+`;
+
+const TextStyled = styled.p`
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 400;
+  font-family: "Roboto", sans-serif;
+  color: #16A34A;
+`;
+
+const ContainerPostStyled = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+border: 1px solid #E5E7EB;
+width: 1200px;
+border-radius: 4px;
+height: 98px;
+margin: 0 auto;
+background-color: #FFFFFF;
+margin-top: 25px;
+`;
+
+const TextAlurriteStyled = styled.h1`
+font-size: 16px;
+line-height: 24px;
+font-weight: 400;
+font-family: "Roboto", sans-serif;
+color: #6B7280;
+margin-left: 20px;
+margin-top: 20px;
+`;
+
+const EmailStyled = styled.h1`
+font-size: 14px;
+line-height: 20px;
+font-weight: 400;
+font-family: "Roboto", sans-serif;
+color: #0EA5E9;
+margin-left: 20px;
+margin-bottom: 10px;
+`;
+
 function Home() {
+  const maxLenght = 255;
+
   const user = useSelector(selectUser);
-  console.log(user);
+
+  const [textArea, setTextArea] = useState('');
+  const [saveText, setSavedText] = useState([]);
+
+  const handleChange = (event) => {
+    setTextArea(event.target.value);
+  };
+
+  const remainsCharacters = maxLenght - textArea.length;
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setSavedText((prevWords) => [...prevWords, textArea]);
+    setTextArea('');
+  };
 
   return (
-    <form>
-      <h1>{user.userData.login}</h1>
-      <h3>Alurite agora mesmo...</h3>
-      <textarea />
-      <button>aluritar</button>
-    </form>
+    <>
+      <Header />
+      <ContainerSginStyled>
+        <StyledForm>
+          <TitleStyled>Alurite agora mesmo...</TitleStyled>
+          <StyledTextarea
+            id="myTextarea"
+            name="myTextarea"
+            rows="4"
+            maxLength="255"
+            value={ textArea }
+            onChange={ handleChange }
+          />
+          <ContainerStyled>
+            <TextStyled>
+              Você ainda pode digitar
+              {' '}
+              {remainsCharacters}
+              {' '}
+              caracteres
+            </TextStyled>
+            <ButtonStyled onClick={ handleClick }>aluritar</ButtonStyled>
+          </ContainerStyled>
+        </StyledForm>
+
+        {saveText.map((test, index) => {
+          return (
+            <ContainerPostStyled key={ index }>
+              <TextAlurriteStyled>{test}</TextAlurriteStyled>
+              <EmailStyled>{user.userData.login}</EmailStyled>
+            </ContainerPostStyled>
+          );
+        })}
+
+      </ContainerSginStyled>
+    </>
   );
 }
 
